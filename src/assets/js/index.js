f280a0bf29495db74cdf36e781b25e3df213ad4f
natar1defou <107755508+natar1defou@users.noreply.github.com>
2022-08-01 13:28:05 +0200

    mise a jour text

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -18,8 +18,8 @@ class Splash {
     async startAnimation() {
         let splashes = [
             { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Un code supprimé est un code débogué.", "author": "Kirua" },
+            { "message": "Le copier-coller a été programmé par des programmeurs", "author": "Kirua" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
